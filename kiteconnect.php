1aa654a9de3cd4ed2d3611c63e8b616c98fcc1c1
Kailash Nadh <kailash.nadh@gmail.com>
2016-05-03 17:24:18 +0530

    Added version

--- kiteconnect.php
+++ kiteconnect.php
@@ -3,7 +3,7 @@
 /**
 Kite Connect API client for PHP -- [kite.trade](https://kite.trade) | [Download from Github](https://github.com/rainmattertech/phpkiteconnect)
 
-Rainmatter (c) 2016
+Rainmatter (c) 2016. Version 1.2
 
 License
 -------
